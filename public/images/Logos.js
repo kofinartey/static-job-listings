966dc75a54b868f4393ecfccf60ce76e24839cb9
Kofi Nartey <eugeneahulu15@live.com>
2021-07-13 21:33:30 +0000

    copied images into public folder

---
+++ public/images/Logos.js
@@ -0,0 +1,25 @@
+import account from "./account.svg";
+import eyecam from "./eyecam-co.svg";
+import faceit from "./faceit.svg";
+import insure from "./insure.svg";
+import loop from "./loop-studios.svg";
+import manage from "./manage.svg";
+import myhome from "./myhome.svg";
+import photosnap from "./photosnap.svg";
+import shortly from "./shortly.svg";
+import theair from "./the-air-filter-company.svg";
+
+const logos = [
+  account,
+  eyecam,
+  faceit,
+  insure,
+  loop,
+  manage,
+  myhome,
+  photosnap,
+  shortly,
+  theair,
+];
+
+export default logos;
